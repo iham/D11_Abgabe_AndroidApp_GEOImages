85fe59b2ef36a336b4e432abaaf9d943f3beef5a
Markus Hilbert <markus.hilbert@iham.at>
2024-01-20 12:59:34 +0100

    initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,19 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "D11_Abgabe_AndroidApp_GEOImages"
+include(":app")
+include(":app")
+ 
