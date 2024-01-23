da330e7be4cc9f3dd288f140d41ed1d4388779d3
Markus Hilbert <markus.hilbert@iham.at>
2024-01-23 16:36:25 +0100

    refactored menu creation.

--- app/src/main/java/com/example/d11_abgabe_androidapp_geoimages/BaseActivity.kt
+++ app/src/main/java/com/example/d11_abgabe_androidapp_geoimages/BaseActivity.kt
@@ -2,6 +2,7 @@ package com.example.d11_abgabe_androidapp_geoimages
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
@@ -15,8 +16,8 @@ abstract class BaseActivity : AppCompatActivity() {
         richNoteDao.getAll()
     }
 
+    abstract var menuItems: List<Int>
     abstract val contentView: Int
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(contentView)
@@ -24,7 +25,8 @@ abstract class BaseActivity : AppCompatActivity() {
         setSupportActionBar(findViewById(R.id.toolbar))
 
         // set listeners for navigation
-        findViewById<BottomNavigationView>(R.id.navigation).setOnItemSelectedListener { item ->
+        val nav = findViewById<BottomNavigationView>(R.id.navigation)
+        nav.setOnItemSelectedListener { item ->
             when (item.itemId) {
                 R.id.list -> startActivity(Intent(this, ListActivity::class.java))
                 R.id.grid -> startActivity(Intent(this, GridActivity::class.java))
@@ -33,4 +35,13 @@ abstract class BaseActivity : AppCompatActivity() {
             true
         }
     }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu items; this adds items to the action bar if it is present.
+        menuItems.forEach {
+            menuItem -> menuInflater.inflate(menuItem, menu)
+        }
+        return true
+    }
+
 }
--- app/src/main/java/com/example/d11_abgabe_androidapp_geoimages/GridActivity.kt
+++ app/src/main/java/com/example/d11_abgabe_androidapp_geoimages/GridActivity.kt
@@ -7,24 +7,16 @@ import android.widget.Toast
 
 class GridActivity : BaseActivity() {
 
-    // Using https://kotlinlang.org/docs/delegated-properties.html#lazy-properties
-    private val richNoteDao: RichNoteDao by lazy {
-        RichNotesDB.getInstance(this).richNoteDao
-    }
     override val contentView = R.layout.activity_grid
+    override var menuItems = listOf(
+        R.menu.sort,
+        R.menu.add,
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         // TODO get rich notes
-        val richNotes = richNoteDao.getAll()
-        Toast.makeText(this, "richNotes in DB: ${richNotes.size}", Toast.LENGTH_SHORT).show()
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.sort, menu)
-        menuInflater.inflate(R.menu.add, menu)
-        return true
+        // Toast.makeText(this, "richNotes in DB: ${richNotes.size}", Toast.LENGTH_SHORT).show()
     }
 }
