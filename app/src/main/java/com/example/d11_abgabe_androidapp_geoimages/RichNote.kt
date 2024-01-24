ea1120fb7ec0daa3124643e700490cebbe54b4bf
Markus Hilbert <markus.hilbert@iham.at>
2024-01-24 17:20:28 +0100

    added grid receiver and viewholder

--- app/src/main/java/com/example/d11_abgabe_androidapp_geoimages/RichNote.kt
+++ app/src/main/java/com/example/d11_abgabe_androidapp_geoimages/RichNote.kt
@@ -5,6 +5,7 @@ import androidx.room.Dao
 import androidx.room.Database
 import androidx.room.Delete
 import androidx.room.Entity
+import androidx.room.Insert
 import androidx.room.PrimaryKey
 import androidx.room.Query
 import androidx.room.Room
